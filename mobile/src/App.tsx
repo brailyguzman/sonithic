51c6dd23efadd0c23ada73ee37ca52fa9ee199c5
Braily Guzman <brailyg@outlook.com>
2024-12-23 20:59:02 -0500

    Implement Navigation

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,20 +1,95 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FC, useContext } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { ThemeContext, ThemeProvider } from './context/ThemeContext';
+import { styleVars } from './App.constants';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIconName } from './App.types';
+import Search from './components/Search/Search';
+import Library from './components/Library/Library';
+import Settings from './components/Settings/Settings';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+const App: FC = () => {
+    const themeContext = useContext(ThemeContext);
+
+    if (!themeContext) {
+        throw new Error('ThemeContext is null');
+    }
+
+    const { colors } = themeContext;
+
     return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
+        <SafeAreaView
+            style={[styles.container, { backgroundColor: colors.background }]}
+        >
             <StatusBar style="auto" />
-        </View>
+            <NavigationContainer>
+                <Tab.Navigator
+                    screenOptions={({ route }) => ({
+                        tabBarIcon: ({ focused, color, size }) => {
+                            let iconName: MaterialIconName;
+
+                            switch (route.name) {
+                                case 'Search':
+                                    iconName = 'search';
+                                    break;
+                                case 'Library':
+                                    iconName = 'library-music';
+                                    break;
+                                case 'Settings':
+                                    iconName = 'settings';
+                                    break;
+                                default:
+                                    iconName = 'library-music';
+                            }
+
+                            return (
+                                <MaterialIcons
+                                    name={iconName}
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        },
+                        tabBarActiveTintColor: colors.accent,
+                        tabBarInactiveTintColor: colors.secondary,
+                        headerShown: false,
+                        tabBarStyle: {
+                            backgroundColor: colors.background,
+                            borderTopWidth: 1,
+                            borderColor: colors.border,
+                        },
+                    })}
+                >
+                    <Tab.Screen name="Library" component={Library} />
+                    <Tab.Screen name="Search" component={Search} />
+                    <Tab.Screen name="Settings" component={Settings} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </SafeAreaView>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+    },
+    content: {
+        flex: 1,
+        padding: styleVars.gutters,
     },
 });
+
+const AppContainer: FC = () => {
+    return (
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    );
+};
+
+export default AppContainer;
