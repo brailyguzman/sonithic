a88053f4577dbe715dd05aefcc8bd62b2f5df0bd
Braily Guzman <brailyg@outlook.com>
2024-12-23 20:54:44 -0500

    Update colorscheme

--- mobile/src/App.constants.ts
+++ mobile/src/App.constants.ts
@@ -2,19 +2,23 @@ import { IColorContainer } from './App.types';
 
 export const colors: IColorContainer = {
     dark: {
-        text: '#ebede8',
-        background: '#060605',
-        primary: '#b5bdad',
-        secondary: '#4b5d5d',
-        accent: '#8e9ba4',
+        text: '#def4de',
+        background: '#030b03',
+        primary: '#86ea84',
+        secondary: '#149e11',
+        accent: '#1df518',
         border: 'rgba(255, 255, 255, 0.1)',
     },
     light: {
-        text: '#151712',
-        background: '#fafaf9',
-        primary: '#4a5242',
-        secondary: '#a2b4b4',
-        accent: '#5b6871',
+        text: '#0b220b',
+        background: '#f3fcf3',
+        primary: '#167a15',
+        secondary: '#64ef62',
+        accent: '#0de60a',
         border: 'rgba(0, 0, 0, 0.1)',
     },
+};
+
+export const styleVars = {
+    gutters: 16,
 };
