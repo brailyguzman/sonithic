505457a0f831c17e5903eb49096717fb91579d8f
Braily Guzman <brailyg@outlook.com>
2024-12-23 20:54:30 -0500

    Remove footer component

--- mobile/src/components/Footer/Footer.types.ts
+++
@@ -1,8 +0,0 @@
-import { MaterialIcons } from '@expo/vector-icons';
-
-type MaterialIconName = React.ComponentProps<typeof MaterialIcons>['name'];
-
-export interface IFooterButton {
-    label: string;
-    icon: MaterialIconName;
-}
